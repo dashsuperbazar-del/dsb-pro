50cccc555f9fe5b213fafd6eafb7f78953a62316
dashsuperbazar-del <dashsuperbazar@gmail.com>
2026-09-08 17:06:20 +0530

    feat: export Phase 3 adapters

--- packages/adapters/src/index.ts
+++ packages/adapters/src/index.ts
@@ -1,37 +1,11 @@
 export { getSupabaseClient } from './client';
 export type { ErrorClass } from './errors';
 export { classifyError, errorMessage } from './errors';
-export {
-  signUp,
-  signIn,
-  signOut,
-  getSession,
-  onAuthStateChange,
-  isEmailVerified,
-  resendVerificationEmail,
-  resetPasswordForEmail,
-} from './auth';
+export { signUp, signIn, signOut, getSession, onAuthStateChange, isEmailVerified, resendVerificationEmail, resetPasswordForEmail } from './auth';
 export type { Session } from './auth';
 export type { Membership, Invite, TenantUser } from './tenancy';
-export {
-  createTenant,
-  getDefaultShopId,
-  createInvite,
-  revokeInvite,
-  acceptInvite,
-  setUserRole,
-  setUserStatus,
-  removeTenantUser,
-  getCurrentMembership,
-  listTenantUsers,
-  listInvites,
-} from './tenancy';
+export { createTenant, getDefaultShopId, createInvite, revokeInvite, acceptInvite, setUserRole, setUserStatus, removeTenantUser, getCurrentMembership, listTenantUsers, listInvites } from './tenancy';
 export type { Device } from './devices';
-export {
-  getOrCreateDeviceId,
-  guessDeviceLabel,
-  registerCurrentDevice,
-  listDevices,
-  renameDevice,
-  revokeDevice,
-} from './devices';
+export { getOrCreateDeviceId, guessDeviceLabel, registerCurrentDevice, listDevices, renameDevice, revokeDevice } from './devices';
+export type { Item, Party, StockRow, PurchaseLine } from './masterData';
+export { listItems, listParties, createItem, createParty, listStock, postPurchase, voidPurchase, archiveMaster, uploadItemImage, purgeItemImage } from './masterData';
